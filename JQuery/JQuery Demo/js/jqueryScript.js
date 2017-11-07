6ef093d19d45435736521380b7f3b5ff21f80920
DESKTOP-IGHA7RH\Adrian Gamarra <adriatogi@gmail.com>
2017-11-06 21:40:55 -0800

    C4 - Selecting with jQuery, Section 16

--- JQuery/JQuery Demo/js/jqueryScript.js
+++ JQuery/JQuery Demo/js/jqueryScript.js
@@ -1,1 +1,7 @@
-$("li").css("color", "green");
+$("li").css("color", "black");
+$("h1").css("font-size", "50px");
+$("li").css({
+    fontSize: "15px",
+    border: "1px dashed DeepSkyBlue",
+    background: "rgba(0, 0, 250, 0.25)"
+})
