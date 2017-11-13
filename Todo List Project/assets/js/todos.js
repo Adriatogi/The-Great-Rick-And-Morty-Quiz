86323f4d32e7764b577c3076d753f8ef83619874
DESKTOP-1S273VA\aggam <aggaming86@gmail.com>
2017-11-12 20:28:12 -0800

    C9 - Finished Todo Project

--- Todo List Project/assets/js/todos.js
+++ Todo List Project/assets/js/todos.js
@@ -1,5 +1,23 @@
 //check off specific Todos by clicking
-$("li").on("click", function() {
+$("ul").on("click", "li", function() {
     $(this).toggleClass("completed");
 })
 
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").on("keypress", function(event){
+    if (event.which === 13) {
+        var todoText = $(this).val(); //grabbing text from input
+        $(this).val(""); // clear input
+        $("ul").append("<li><span><i class='fa fa-trash' aria-hidden='true'></i></span> " + todoText + "</li>");//create a new li to add to ul
+    }
+});
+
+$(".fa-plus").on("click", function(){
+    $("input[type='text']").fadeToggle();
+})
